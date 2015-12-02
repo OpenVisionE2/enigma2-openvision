bfc46ad7763e9d30317ab18dcc13bfe8ba44b654
Captain <captain.onboard@web.de>
2015-12-02 06:48:03 +0100

    [h265] [TSPlayback] Fix UHD TS playback on fastForward and rewind.

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -1633,7 +1633,17 @@ RESULT eDVBServicePlay::setFastForward_internal(int ratio, bool final_seek)
 	{
 		eDebug("[eDVBServicePlay] setFastForward setting cue skipmode to %d", skipmode);
 		if (m_cue)
-			m_cue->setSkipmode(skipmode * 90000); /* convert to 90000 per second */
+		{
+			long long _skipmode = skipmode;
+			if (!m_timeshift_active && (m_current_video_pid_type == eDVBServicePMTHandler::videoStream::vtH265_HEVC))
+			{
+				if (ratio < 0)
+					_skipmode = skipmode * 3;
+				else
+					_skipmode = skipmode * 4;
+			}
+			m_cue->setSkipmode(_skipmode * 90000); /* convert to 90000 per second */
+		}
 	}
 
 	m_skipmode = skipmode;
@@ -3243,6 +3253,7 @@ void eDVBServicePlay::updateDecoder(bool sendSeekableStateChanged)
 #else
 		m_decoder->setVideoPID(vpid, vpidtype);
 #endif
+		m_current_video_pid_type = vpidtype;
 		m_have_video_pid = (vpid > 0 && vpid < 0x2000);
 
 		if (!m_noaudio)
