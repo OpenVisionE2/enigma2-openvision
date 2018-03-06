5ffab64ed7b6a84a146e3bf3b5cb13e38e1db80a
Peter Urbanec <bitbucket.org@urbanec.net>
2018-03-07 05:09:55 +1100

    Add HDMI input recording settings

--- mytest.py
+++ mytest.py
@@ -554,6 +554,10 @@ profile("AVSwitch")
 import Components.AVSwitch
 Components.AVSwitch.InitAVSwitch()
 
+profile("HdmiRecord")
+import Components.HdmiRecord
+Components.HdmiRecord.InitHdmiRecord()
+
 profile("RecordingConfig")
 import Components.RecordingConfig
 Components.RecordingConfig.InitRecordingConfig()
