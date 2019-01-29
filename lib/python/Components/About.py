c08317fab9c0258cb28af98d8a769bc9b6d31c0c
Persian Prince <persianpros@yahoo.com>
2019-01-29 11:43:57 +0330

    Update About.py

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -130,9 +130,10 @@ def getDriverInstalledDate():
 		try:
 			if getBoxType() in ('dm800','dm8000'):
 				driver = [x.split("-")[-2:-1][0][-9:] for x in open(glob("/var/lib/opkg/info/*-dvb-modules-*.control")[0], "r") if x.startswith("Version:")][0]
+				return  "%s-%s-%s" % (driver[:4], driver[4:6], driver[6:])
 			else
 				driver = [x.split("-")[-2:-1][0][-8:] for x in open(glob("/var/lib/opkg/info/*-dvb-modules-*.control")[0], "r") if x.startswith("Version:")][0]
-			return  "%s-%s-%s" % (driver[:4], driver[4:6], driver[6:])
+				return  "%s-%s-%s" % (driver[:4], driver[4:6], driver[6:])
 		except:
 			try:
 				driver = [x.split("Version:") for x in open(glob("/var/lib/opkg/info/*-dvb-proxy-*.control")[0], "r") if x.startswith("Version:")][0]
