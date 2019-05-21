0df1019d7f82393e115eb6def85b6c97c0fe9d4e
Persian Prince <persianpros@yahoo.com>
2019-05-21 20:57:53 +0430

    config.py: Support extra_args

--- lib/python/Components/config.py
+++ lib/python/Components/config.py
@@ -29,12 +29,13 @@ from time import localtime, strftime
 #
 class ConfigElement(object):
 	def __init__(self):
++		self.extra_args = {}
 		self.saved_value = None
 		self.save_forced = False
 		self.last_value = None
 		self.save_disabled = False
-		self.__notifiers = None
-		self.__notifiers_final = None
+		self.__notifiers = {}
+		self.__notifiers_final = {}
 		self.enabled = True
 		self.callNotifiersOnSaveAndCancel = False
 
@@ -106,15 +107,31 @@ class ConfigElement(object):
 	def changed(self):
 		if self.__notifiers:
 			for x in self.notifiers:
-				x(self)
+				try:
+					if self.extra_args and self.extra_args[x]:
+						x(self, self.extra_args[x])
+					else:
+						x(self)
+				except:
+					x(self)
 
 	def changedFinal(self):
 		if self.__notifiers_final:
 			for x in self.notifiers_final:
-				x(self)
+				try:
+					if self.extra_args and self.extra_args[x]:
+						x(self, self.extra_args[x])
+					else:
+						x(self)
+				except:
+					x(self)
 
-	def addNotifier(self, notifier, initial_call=True, immediate_feedback=True):
+	def addNotifier(self, notifier, initial_call=True, immediate_feedback=True, extra_args=None):
+		if not extra_args: extra_args = []
 		assert callable(notifier), "notifiers must be callable"
+		try:
+			self.extra_args[notifier] = extra_args
+		except: pass
 		if immediate_feedback:
 			self.notifiers.append(notifier)
 		else:
@@ -128,7 +145,10 @@ class ConfigElement(object):
 		#    (though that's not so easy to detect.
 		#     the entry could just be new.)
 		if initial_call:
-			notifier(self)
+			if extra_args:
+				notifier(self,extra_args)
+			else:
+				notifier(self)
 
 	def removeNotifier(self, notifier):
 		notifier in self.notifiers and self.notifiers.remove(notifier)
@@ -1093,7 +1113,14 @@ class ConfigNumber(ConfigText):
 		ConfigText.__init__(self, str(default), fixed_size=False)
 
 	def getValue(self):
-		return int(self.text)
+		try:
+			return int(self.text)
+		except ValueError:
+			if self.text == "true":
+				self.text = "1"
+			else:
+				self.text = str(default)
+			return int(self.text)
 
 	def setValue(self, val):
 		self.text = str(val)
@@ -1188,12 +1215,15 @@ class ConfigSlider(ConfigElement):
 		self.max = limits[1]
 		self.increment = increment
 
-	def checkValues(self):
-		if self.value < self.min:
-			self.value = self.min
-
-		if self.value > self.max:
-			self.value = self.max
+	def checkValues(self, value = None):
+		if value is None:
+			value = self.value
+		if value < self.min:
+			value = self.min
+		if value > self.max:
+			value = self.max
+		if self.value != value:		#avoid call of setter if value not changed
+			self.value = value
 
 	def handleKey(self, key):
 		if key == KEY_LEFT:
