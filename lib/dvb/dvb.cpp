a98624087e23e49003eebe7b7e1d8fd64cc2122a
persianpros <persianpros@yahoo.com>
2019-09-22 12:00:40 +0330

    dvb.cpp: Fix build for oldapi like dm800

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -331,7 +331,18 @@ eDVBUsbAdapter::eDVBUsbAdapter(int nr)
 
 	struct dtv_properties props;
 	struct dtv_property prop[1];
-#if defined(DTV_ENUM_DELSYS) || !defined(HAVE_OLDE2_API)
+#ifdef HAVE_OLDE2_API
+#if defined DTV_ENUM_DELSYS
+	prop[0].cmd = DTV_ENUM_DELSYS;
+	memset(prop[0].u.buffer.data, 0, sizeof(prop[0].u.buffer.data));
+	prop[0].u.buffer.len = 0;
+	props.num = 1;
+	props.props = prop;
+
+	if (ioctl(frontend, FE_GET_PROPERTY, &props) < 0)
+		eDebug("[eDVBUsbAdapter] FE_GET_PROPERTY DTV_ENUM_DELSYS failed %m");
+#endif
+#else
 	prop[0].cmd = DTV_ENUM_DELSYS;
 	memset(prop[0].u.buffer.data, 0, sizeof(prop[0].u.buffer.data));
 	prop[0].u.buffer.len = 0;
@@ -440,7 +451,12 @@ eDVBUsbAdapter::eDVBUsbAdapter(int nr)
 	ioctl(vtunerFd, VTUNER_SET_NAME, name);
 	ioctl(vtunerFd, VTUNER_SET_TYPE, type);
 	ioctl(vtunerFd, VTUNER_SET_FE_INFO, &fe_info);
-#if defined(DTV_ENUM_DELSYS) || !defined(HAVE_OLDE2_API)
+#ifdef HAVE_OLDE2_API
+#if defined DTV_ENUM_DELSYS
+	if (prop[0].u.buffer.len > 0)
+		ioctl(vtunerFd, VTUNER_SET_DELSYS, prop[0].u.buffer.data);
+#endif
+#else
 	if (prop[0].u.buffer.len > 0)
 		ioctl(vtunerFd, VTUNER_SET_DELSYS, prop[0].u.buffer.data);
 #endif
@@ -774,10 +790,18 @@ bool eDVBResourceManager::frontendIsCompatible(int index, const char *type)
 			}
 			else if (!strcmp(type, "DVB-C"))
 			{
-#if DVB_API_VERSION > 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR >= 6 || defined(SYS_DVBC_ANNEX_A)
+#ifdef HAVE_OLDE2_API
+#if DVB_API_VERSION > 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR >= 6
 				return i->m_frontend->supportsDeliverySystem(SYS_DVBC_ANNEX_A, false) || i->m_frontend->supportsDeliverySystem(SYS_DVBC_ANNEX_C, false);
 #else
 				return i->m_frontend->supportsDeliverySystem(SYS_DVBC_ANNEX_AC, false);
+#endif
+#else
+#if defined SYS_DVBC_ANNEX_A
+				return i->m_frontend->supportsDeliverySystem(SYS_DVBC_ANNEX_A, false) || i->m_frontend->supportsDeliverySystem(SYS_DVBC_ANNEX_C, false);
+#else
+				return i->m_frontend->supportsDeliverySystem(SYS_DVBC_ANNEX_AC, false);
+#endif
 #endif
 			}
 			else if (!strcmp(type, "ATSC"))
@@ -838,11 +862,20 @@ void eDVBResourceManager::setFrontendType(int index, const char *types)
 			}
 			else if (type == "DVB-C")
 			{
-#if DVB_API_VERSION > 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR >= 6 || defined(SYS_DVBC_ANNEX_A)
+#ifdef HAVE_OLDE2_API
+#if DVB_API_VERSION > 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR >= 6
 				whitelist.push_back(SYS_DVBC_ANNEX_A);
 				whitelist.push_back(SYS_DVBC_ANNEX_C);
 #else
 				whitelist.push_back(SYS_DVBC_ANNEX_AC);
+#endif
+#else
+#if defined SYS_DVBC_ANNEX_A
+				whitelist.push_back(SYS_DVBC_ANNEX_A);
+				whitelist.push_back(SYS_DVBC_ANNEX_C);
+#else
+				whitelist.push_back(SYS_DVBC_ANNEX_AC);
+#endif
 #endif
 			}
 			else if (type == "ATSC")
