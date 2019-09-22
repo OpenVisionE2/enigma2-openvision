06494f0873952268a036ac6a48ccde4fffe512aa
persianpros <persianpros@yahoo.com>
2019-09-23 00:02:59 +0330

    Get rid of CIHelper support for now as it seems new changes won't need it.

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -113,7 +113,6 @@ SystemInfo["CanDownmixDTS"] = fileHas("/proc/stb/audio/dts_choices","downmix")
 SystemInfo["CanDownmixAAC"] = fileHas("/proc/stb/audio/aac_choices","downmix")
 SystemInfo["HDMIAudioSource"] = fileCheck("/proc/stb/hdmi/audio_source")
 SystemInfo["SmallFlash"] = fileExists("/etc/smallflash")
-SystemInfo["CIHelper"] = fileExists("/usr/bin/cihelper")
 SystemInfo["HaveCISSL"] = fileCheck("/etc/ssl/certs/customer.pem") and fileCheck("/etc/ssl/certs/device.pem")
 SystemInfo["CanChangeOsdAlpha"] = access("/proc/stb/video/alpha", R_OK) and True or False
 SystemInfo["ScalerSharpness"] = fileExists("/proc/stb/vmpeg/0/pep_scaler_sharpness")
