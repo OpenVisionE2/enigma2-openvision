6157496a06d9e26e5b723bb4f6eb099b1406c2df
norhap <alvaro@elfogondesegovia.com>
2019-10-05 20:40:28 +0200

    Add News Screens SystemNetworkInfo/Devices in Information thanks to ViX
    new information screens are added:
    menu-> information-> Devices and Network.
    * Network Servers information.
    * information tuners detected.
    * information devices detected.

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -3,7 +3,7 @@ from Screens.MessageBox import MessageBox
 from Components.config import config
 from Components.ActionMap import ActionMap
 from Components.Sources.StaticText import StaticText
-from Components.Harddisk import harddiskmanager
+from Components.Harddisk import harddiskmanager, Harddisk
 from Components.NimManager import nimmanager
 from Components.About import about
 from Components.ScrollLabel import ScrollLabel
@@ -16,6 +16,10 @@ from Tools.Directories import fileExists, fileHas
 from Components.GUIComponent import GUIComponent
 import skin, os, boxbranding
 from Components.Console import Console
+from Components.Pixmap import MultiPixmap
+from Components.Network import iNetwork
+from Components.SystemInfo import SystemInfo
+from re import search
 
 class About(Screen):
 	def __init__(self, session):
@@ -202,6 +206,429 @@ class About(Screen):
 
 	def showTroubleshoot(self):
 		self.session.open(Troubleshoot)
+		
+class Devices(Screen):
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		screentitle = _("Devices")
+		title = screentitle
+		Screen.setTitle(self, title)
+		self["TunerHeader"] = StaticText(_("Detected tuners:"))
+		self["HDDHeader"] = StaticText(_("Detected devices:"))
+		self["MountsHeader"] = StaticText(_("Network servers:"))
+		self["nims"] = StaticText()
+		for count in (0, 1, 2, 3):
+			self["Tuner" + str(count)] = StaticText("")
+		self["hdd"] = StaticText()
+		self["mounts"] = StaticText()
+		self.list = []
+		self.activityTimer = eTimer()
+		self.activityTimer.timeout.get().append(self.populate2)
+		self["key_red"] = Button(_("Close"))
+		self["actions"] = ActionMap(["SetupActions", "ColorActions", "TimerEditActions"],
+									{
+										"cancel": self.close,
+										"ok": self.close,
+										"red": self.close,
+									})
+		self.onLayoutFinish.append(self.populate)
+
+	def populate(self):
+		self.mountinfo = ''
+		self["actions"].setEnabled(False)
+		scanning = _("Please wait while scanning for devices...")
+		self["nims"].setText(scanning)
+		for count in (0, 1, 2, 3):
+			self["Tuner" + str(count)].setText(scanning)
+		self["hdd"].setText(scanning)
+		self['mounts'].setText(scanning)
+		self.activityTimer.start(1)
+
+	def populate2(self):
+		self.activityTimer.stop()
+		self.Console = Console()
+		niminfo = ""
+		nims = nimmanager.nimListCompressed()
+		for count in range(len(nims)):
+			if niminfo:
+				niminfo += "\n"
+			niminfo += nims[count]
+		self["nims"].setText(niminfo)
+
+		nims = nimmanager.nimList()
+		if len(nims) <= 4 :
+			for count in (0, 1, 2, 3):
+				if count < len(nims):
+					self["Tuner" + str(count)].setText(nims[count])
+				else:
+					self["Tuner" + str(count)].setText("")
+		else:
+			desc_list = []
+			count = 0
+			cur_idx = -1
+			while count < len(nims):
+				data = nims[count].split(":")
+				idx = data[0].strip('Tuner').strip()
+				desc = data[1].strip()
+				if desc_list and desc_list[cur_idx]['desc'] == desc:
+					desc_list[cur_idx]['end'] = idx
+				else:
+					desc_list.append({'desc' : desc, 'start' : idx, 'end' : idx})
+					cur_idx += 1
+				count += 1
+
+			for count in (0, 1, 2, 3):
+				if count < len(desc_list):
+					if desc_list[count]['start'] == desc_list[count]['end']:
+						text = "Tuner %s: %s" % (desc_list[count]['start'], desc_list[count]['desc'])
+					else:
+						text = "Tuner %s-%s: %s" % (desc_list[count]['start'], desc_list[count]['end'], desc_list[count]['desc'])
+				else:
+					text = ""
+
+				self["Tuner" + str(count)].setText(text)
+
+		self.list = []
+		list2 = []
+		f = open('/proc/partitions', 'r')
+		for line in f.readlines():
+			parts = line.strip().split()
+			if not parts:
+				continue
+			device = parts[3]
+			if SystemInfo["HasRootSubdir"]:
+				if not search('sd[a-z][1-9]', device):
+					continue
+			else:	
+				if not search('sd[a-z][1-9]', device) and not search('mmcblk[0-9]p[1-9]', device):
+					continue
+			if SystemInfo["HasMMC"] and pathExists("/dev/sda4") and search('sd[a][1-4]', device):
+				continue
+			if device in list2:
+				continue
+
+			mount = '/dev/' + device
+			f = open('/proc/mounts', 'r')
+			for line in f.readlines():
+				if device in line:
+					parts = line.strip().split()
+					mount = str(parts[1])
+					break
+			f.close()
+
+			if not mount.startswith('/dev/'):
+				size = Harddisk(device).diskSize()
+				free = Harddisk(device).free()
+
+				if ((float(size) / 1024) / 1024) >= 1:
+					sizeline = _("Size: ") + str(round(((float(size) / 1024) / 1024), 2)) + _("TB")
+				elif (size / 1024) >= 1:
+					sizeline = _("Size: ") + str(round((float(size) / 1024), 2)) + _("GB")
+				elif size >= 1:
+					sizeline = _("Size: ") + str(size) + _("MB")
+				else:
+					sizeline = _("Size: ") + _("unavailable")
+
+				if ((float(free) / 1024) / 1024) >= 1:
+					freeline = _("Free: ") + str(round(((float(free) / 1024) / 1024), 2)) + _("TB")
+				elif (free / 1024) >= 1:
+					freeline = _("Free: ") + str(round((float(free) / 1024), 2)) + _("GB")
+				elif free >= 1:
+					freeline = _("Free: ") + str(free) + _("MB")
+				else:
+					freeline = _("Free: ") + _("full")
+				if mount.find('mmc') == -1 and mount.find('boot') == -1:
+					self.list.append(mount + '\t' + sizeline + ' \t' + freeline)
+			else:
+				print "MOUNT:", mount
+				if mount.find('mmc') == -1:
+					self.list.append(mount + '\t' + _('Not mounted'))
+
+			list2.append(device)
+		self.list = '\n'.join(self.list)
+		self["hdd"].setText(self.list)
+
+		self.Console.ePopen("df -mh | grep -v '^Filesystem'", self.Stage1Complete)
+
+	def Stage1Complete(self, result, retval, extra_args=None):
+		result = result.replace('\n                        ', ' ').split('\n')
+		self.mountinfo = ""
+		for line in result:
+			self.parts = line.split()
+			if line and self.parts[0] and (self.parts[0].startswith('192') or self.parts[0].startswith('//192')):
+				line = line.split()
+				ipaddress = line[0]
+				mounttotal = line[1]
+				mountfree = line[3]
+				if self.mountinfo:
+					self.mountinfo += "\n"
+				self.mountinfo += "%s (%sB, %sB %s)" % (ipaddress, mounttotal, mountfree, _("free"))
+
+		if self.mountinfo:
+			self["mounts"].setText(self.mountinfo)
+		else:
+			self["mounts"].setText(_('none'))
+		self["actions"].setEnabled(True)
+
+class SystemNetworkInfo(Screen):
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		screentitle = _("Network")
+		title = screentitle
+		Screen.setTitle(self, title)
+		self.skinName = ["SystemNetworkInfo", "WlanStatus"]
+		self["LabelBSSID"] = StaticText()
+		self["LabelESSID"] = StaticText()
+		self["LabelQuality"] = StaticText()
+		self["LabelSignal"] = StaticText()
+		self["LabelBitrate"] = StaticText()
+		self["LabelEnc"] = StaticText()
+		self["BSSID"] = StaticText()
+		self["ESSID"] = StaticText()
+		self["quality"] = StaticText()
+		self["signal"] = StaticText()
+		self["bitrate"] = StaticText()
+		self["enc"] = StaticText()
+
+		self["IFtext"] = StaticText()
+		self["IF"] = StaticText()
+		self["Statustext"] = StaticText()
+		self["statuspic"] = MultiPixmap()
+		self["statuspic"].setPixmapNum(1)
+		self["statuspic"].show()
+		self["devicepic"] = MultiPixmap()
+
+		self["AboutScrollLabel"] = ScrollLabel()
+
+		self.iface = None
+		self.createscreen()
+		self.iStatus = None
+
+		if iNetwork.isWirelessInterface(self.iface):
+			try:
+				from Plugins.SystemPlugins.WirelessLan.Wlan import iStatus
+
+				self.iStatus = iStatus
+			except:
+				pass
+			self.resetList()
+			self.onClose.append(self.cleanup)
+
+		self["key_red"] = StaticText(_("Close"))
+
+		self["actions"] = ActionMap(["SetupActions", "ColorActions", "DirectionActions"],
+									{
+										"cancel": self.close,
+										"ok": self.close,
+										"up": self["AboutScrollLabel"].pageUp,
+										"down": self["AboutScrollLabel"].pageDown
+									})
+		self.onLayoutFinish.append(self.updateStatusbar)
+
+	def createscreen(self):
+		self.AboutText = ""
+		self.iface = "eth0"
+		eth0 = about.getIfConfig('eth0')
+		if eth0.has_key('addr'):
+			self.AboutText += _("IP:") + "\t" + eth0['addr'] + "\n"
+			if eth0.has_key('netmask'):
+				self.AboutText += _("Netmask:") + "\t" + eth0['netmask'] + "\n"
+			if eth0.has_key('hwaddr'):
+				self.AboutText += _("MAC:") + "\t" + eth0['hwaddr'] + "\n"
+			self.iface = 'eth0'
+
+		eth1 = about.getIfConfig('eth1')
+		if eth1.has_key('addr'):
+			self.AboutText += _("IP:") + "\t" + eth1['addr'] + "\n"
+			if eth1.has_key('netmask'):
+				self.AboutText += _("Netmask:") + "\t" + eth1['netmask'] + "\n"
+			if eth1.has_key('hwaddr'):
+				self.AboutText += _("MAC:") + "\t" + eth1['hwaddr'] + "\n"
+			self.iface = 'eth1'
+
+		ra0 = about.getIfConfig('ra0')
+		if ra0.has_key('addr'):
+			self.AboutText += _("IP:") + "\t" + ra0['addr'] + "\n"
+			if ra0.has_key('netmask'):
+				self.AboutText += _("Netmask:") + "\t" + ra0['netmask'] + "\n"
+			if ra0.has_key('hwaddr'):
+				self.AboutText += _("MAC:") + "\t" + ra0['hwaddr'] + "\n"
+			self.iface = 'ra0'
+
+		wlan0 = about.getIfConfig('wlan0')
+		if wlan0.has_key('addr'):
+			self.AboutText += _("IP:") + "\t" + wlan0['addr'] + "\n"
+			if wlan0.has_key('netmask'):
+				self.AboutText += _("Netmask:") + "\t" + wlan0['netmask'] + "\n"
+			if wlan0.has_key('hwaddr'):
+				self.AboutText += _("MAC:") + "\t" + wlan0['hwaddr'] + "\n"
+			self.iface = 'wlan0'
+
+		wlan3 = about.getIfConfig('wlan3')
+		if wlan3.has_key('addr'):
+			self.AboutText += _("IP:") + "\t" + wlan3['addr'] + "\n"
+			if wlan3.has_key('netmask'):
+				self.AboutText += _("Netmask:") + "\t" + wlan3['netmask'] + "\n"
+			if wlan3.has_key('hwaddr'):
+				self.AboutText += _("MAC:") + "\t" + wlan3['hwaddr'] + "\n"
+			self.iface = 'wlan3'
+
+		rx_bytes, tx_bytes = about.getIfTransferredData(self.iface)
+		self.AboutText += "\n" + _("Bytes received:") + "\t" + rx_bytes + "\n"
+		self.AboutText += _("Bytes sent:") + "\t" + tx_bytes + "\n"
+
+		self.console = Console()
+		self.console.ePopen('ethtool %s' % self.iface, self.SpeedFinished)
+
+	def SpeedFinished(self, result, retval, extra_args):
+		result_tmp = result.split('\n')
+		for line in result_tmp:
+			if 'Speed:' in line:
+				speed = line.split(': ')[1][:-4]
+				self.AboutText += _("Speed:") + "\t" + speed + _('Mb/s')
+
+		hostname = file('/proc/sys/kernel/hostname').read()
+		self.AboutText += "\n" + _("Hostname:") + "\t" + hostname + "\n"
+		self["AboutScrollLabel"].setText(self.AboutText)
+
+	def cleanup(self):
+		if self.iStatus:
+			self.iStatus.stopWlanConsole()
+
+	def resetList(self):
+		if self.iStatus:
+			self.iStatus.getDataForInterface(self.iface, self.getInfoCB)
+
+	def getInfoCB(self, data, status):
+		self.LinkState = None
+		if data is not None and data:
+			if status is not None:
+# getDataForInterface()->iwconfigFinished() in
+# Plugins/SystemPlugins/WirelessLan/Wlan.py sets fields to boolean False
+# if there is no info for them, so we need to check that possibility
+# for each status[self.iface] field...
+#
+				if self.iface == 'wlan0' or self.iface == 'wlan3' or self.iface == 'ra0':
+# accesspoint is used in the "enc" code too, so we get it regardless
+#
+					if not status[self.iface]["accesspoint"]:
+						accesspoint = _("Unknown")
+					else:
+						if status[self.iface]["accesspoint"] == "Not-Associated":
+							accesspoint = _("Not-Associated")
+							essid = _("No connection")
+						else:
+							accesspoint = status[self.iface]["accesspoint"]
+					if self.has_key("BSSID"):
+						self.AboutText += _('Accesspoint:') + '\t' + accesspoint + '\n'
+
+					if self.has_key("ESSID"):
+						if not status[self.iface]["essid"]:
+							essid = _("Unknown")
+						else:
+							if status[self.iface]["essid"] == "off":
+								essid = _("No connection")
+							else:
+								essid = status[self.iface]["essid"]
+						self.AboutText += _('SSID:') + '\t' + essid + '\n'
+
+					if self.has_key("quality"):
+						if not status[self.iface]["quality"]:
+							quality = _("Unknown")
+						else:
+							quality = status[self.iface]["quality"]
+						self.AboutText += _('Link quality:') + '\t' + quality + '\n'
+
+					if self.has_key("bitrate"):
+						if not status[self.iface]["bitrate"]:
+							bitrate = _("Unknown")
+						else:
+							if status[self.iface]["bitrate"] == '0':
+								bitrate = _("Unsupported")
+							else:
+								bitrate = str(status[self.iface]["bitrate"]) + " Mb/s"
+						self.AboutText += _('Bitrate:') + '\t' + bitrate + '\n'
+
+					if self.has_key("signal"):
+						if not status[self.iface]["signal"]:
+							signal = _("Unknown")
+						else:
+							signal = status[self.iface]["signal"]
+						self.AboutText += _('Signal strength:') + '\t' + signal + '\n'
+
+					if self.has_key("enc"):
+						if not status[self.iface]["encryption"]:
+							encryption = _("Unknown")
+						else:
+							if status[self.iface]["encryption"] == "off":
+								if accesspoint == "Not-Associated":
+									encryption = _("Disabled")
+								else:
+									encryption = _("Unsupported")
+							else:
+								encryption = _("Enabled")
+						self.AboutText += _('Encryption:') + '\t' + encryption + '\n'
+
+					if ((status[self.iface]["essid"] and status[self.iface]["essid"] == "off") or
+					    not status[self.iface]["accesspoint"] or
+					    status[self.iface]["accesspoint"] == "Not-Associated"):
+						self.LinkState = False
+						self["statuspic"].setPixmapNum(1)
+						self["statuspic"].show()
+					else:
+						self.LinkState = True
+						iNetwork.checkNetworkState(self.checkNetworkCB)
+					self["AboutScrollLabel"].setText(self.AboutText)
+
+	def exit(self):
+		self.close(True)
+
+	def updateStatusbar(self):
+		self["IFtext"].setText(_("Network:"))
+		self["IF"].setText(iNetwork.getFriendlyAdapterName(self.iface))
+		self["Statustext"].setText(_("Link:"))
+		if iNetwork.isWirelessInterface(self.iface):
+			self["devicepic"].setPixmapNum(1)
+			try:
+				self.iStatus.getDataForInterface(self.iface, self.getInfoCB)
+			except:
+				self["statuspic"].setPixmapNum(1)
+				self["statuspic"].show()
+		else:
+			iNetwork.getLinkState(self.iface, self.dataAvail)
+			self["devicepic"].setPixmapNum(0)
+		self["devicepic"].show()
+
+	def dataAvail(self, data):
+		self.LinkState = None
+		for line in data.splitlines():
+			line = line.strip()
+			if 'Link detected:' in line:
+				if "yes" in line:
+					self.LinkState = True
+				else:
+					self.LinkState = False
+		if self.LinkState:
+			iNetwork.checkNetworkState(self.checkNetworkCB)
+		else:
+			self["statuspic"].setPixmapNum(1)
+			self["statuspic"].show()
+
+	def checkNetworkCB(self, data):
+		try:
+			if iNetwork.getAdapterAttribute(self.iface, "up") is True:
+				if self.LinkState is True:
+					if data <= 2:
+						self["statuspic"].setPixmapNum(0)
+					else:
+						self["statuspic"].setPixmapNum(1)
+				else:
+					self["statuspic"].setPixmapNum(1)
+			else:
+				self["statuspic"].setPixmapNum(1)
+			self["statuspic"].show()
+		except:
+			pass		
 
 class TranslationInfo(Screen):
 	def __init__(self, session):
