341ceb6b4e710d5212b7015951cc5c38a795f5a0
Persian Prince <persianpros@yahoo.com>
2019-10-14 15:42:49 +0330

    Update StartWizard.py

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -8,7 +8,6 @@ try:
 	from Plugins.SystemPlugins.OSDPositionSetup.overscanwizard import OverscanWizard
 except:
 	OverscanWizard = None
-
 from Components.Pixmap import Pixmap
 from Components.ProgressBar import ProgressBar
 from Components.Label import Label
@@ -16,14 +15,10 @@ from Components.ScrollLabel import ScrollLabel
 from Components.config import config, ConfigBoolean, configfile
 from LanguageSelection import LanguageWizard
 from enigma import eConsoleAppContainer, eTimer, eActionMap
-from Components.SystemInfo import SystemInfo
 import os
 
 config.misc.firstrun = ConfigBoolean(default = True)
-if SystemInfo["SmallFlash"]:
-	config.misc.languageselected = ConfigBoolean(default = False)
-else:
-	config.misc.languageselected = ConfigBoolean(default = True)	
+config.misc.languageselected = ConfigBoolean(default = False)
 config.misc.do_overscanwizard = ConfigBoolean(default = OverscanWizard)
 
 class StartWizard(WizardLanguage, Rc):
