bb3227bce26876f89dd75f52291a076a4e0ad5d8
norhap <alvaro@elfogondesegovia.com>
2019-11-06 03:08:33 +0100

    Add Arch mips32el for Fade out and getBoxType aarch64  OS mio+ 4k
    Added arch mips32el, in these STB I never had problems.
    
    In getBoxType we can add those ARM machines that do not reproduce this problem, for the moment osmio4kplus has been added.
    
    I am testing OS mio + 4k ARMv8 with aarch64 and everything is fine.

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -3,7 +3,7 @@ from Tools.Directories import fileExists, fileCheck, pathExists, fileHas
 from Tools.HardwareInfo import HardwareInfo
 import os
 from os import access, R_OK
-from boxbranding import getDisplayType
+from boxbranding import getDisplayType, getImageArch
 
 SystemInfo = {}
 
@@ -123,7 +123,7 @@ SystemInfo["NCamIsActive"] = SystemInfo["NCamInstalled"] and fileExists("/tmp/.n
 SystemInfo["OpenVisionModule"] = fileCheck("/proc/stb/info/openvision")
 SystemInfo["OLDE2API"] = getBoxType() in ("dm800","su980")
 SystemInfo["7segment"] = getDisplayType() == "7segment"
-SystemInfo["CanFadeOut"] = getBoxBrand() == "dreambox"
+SystemInfo["CanFadeOut"] = getImageArch() == "mips32el" or getBoxType() in ("osmio4kplus") or getBoxBrand() == "dreambox"
 SystemInfo["OSDAnimation"] = fileCheck("/proc/stb/fb/animation_mode")
 SystemInfo["RecoveryMode"] = fileCheck("/proc/stb/fp/boot_mode") and getBoxType() not in ("hd51","h7")
 SystemInfo["AndroidMode"] =  SystemInfo["RecoveryMode"] and getBoxType() == "multibox" or getBoxBrand() in ("hypercube","linkdroid","mecool","wetek") or getBoxType() == "dreamone"
