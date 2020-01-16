6c62a05440d37dc2f7d2c07bab7a456321fb127d
Aliaksandr <sid8796@gmail.com>
2020-01-16 11:52:03 +0300

    [Horizontal Menu] fix enigma2 crashlog
    < 32689.195>   File "/usr/lib/enigma2/python/Components/ActionMap.py", line 57, in action
    < 32689.222>   File "/usr/lib/enigma2/python/Screens/InfoBarGenerics.py", line 977, in mainMenu
    < 32689.228>   File "/usr/lib/enigma2/python/mytest.py", line 319, in openWithCallback
    < 32689.233>     dlg = self.open(screen, *arguments, **kwargs)
    < 32689.234>   File "/usr/lib/enigma2/python/mytest.py", line 329, in open
    < 32689.237>     dlg = self.current_dialog = self.instantiateDialog(screen, *arguments, **kwargs)
    < 32689.238>   File "/usr/lib/enigma2/python/mytest.py", line 266, in instantiateDialog
    < 32689.241>     return self.doInstantiateDialog(screen, arguments, kwargs, self.desktop)
    < 32689.241>   File "/usr/lib/enigma2/python/mytest.py", line 289, in doInstantiateDialog
    < 32689.244>     dlg = screen(self, *arguments, **kwargs)
    < 32689.245>   File "/usr/lib/enigma2/python/Screens/Menu.py", line 998, in __init__
    < 32689.249>   File "/usr/lib/enigma2/python/Screens/Menu.py", line 270, in __init__
    < 32689.253> AttributeError: 'module' object has no attribute 'dom_screens'

--- lib/python/Screens/Menu.py
+++ lib/python/Screens/Menu.py
@@ -267,9 +267,9 @@ class Menu(Screen, ProtectedScreen):
 		# for the skin: first try a menu_<menuID>, then Menu
 		self.skinName = [ ]
 		if self.menuID is not None:
-			if config.usage.menutype.value == 'horzanim' and skin.dom_screens.has_key("Animmain"):
+			if config.usage.menutype.value == 'horzanim' and skin.domScreens.has_key("Animmain"):
 				self.skinName.append('Animmain')
-			elif config.usage.menutype.value == 'horzicon' and skin.dom_screens.has_key("Iconmain"):
+			elif config.usage.menutype.value == 'horzicon' and skin.domScreens.has_key("Iconmain"):
 				self.skinName.append('Iconmain')
 			else:
 				self.skinName.append('menu_' + self.menuID)
@@ -322,14 +322,14 @@ class Menu(Screen, ProtectedScreen):
 			t_history.thistory = str(title) + ' > '
 		else:
 			t_history.thistory = t_history.thistory + str(title) + ' > '
-		if config.usage.menutype.value == 'horzanim' and skin.dom_screens.has_key("Animmain"):
+		if config.usage.menutype.value == 'horzanim' and skin.domScreens.has_key("Animmain"):
 			self['label1'] = StaticText()
 			self['label2'] = StaticText()
 			self['label3'] = StaticText()
 			self['label4'] = StaticText()
 			self['label5'] = StaticText()
 			self.onShown.append(self.openTestA)
-		elif config.usage.menutype.value == 'horzicon' and skin.dom_screens.has_key("Iconmain"):
+		elif config.usage.menutype.value == 'horzicon' and skin.domScreens.has_key("Iconmain"):
 			self['label1'] = StaticText()
 			self['label2'] = StaticText()
 			self['label3'] = StaticText()
