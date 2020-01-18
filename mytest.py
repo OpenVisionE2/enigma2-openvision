c63bd8db6eaca02a5668e77a8448a2ae26102efa
persianpros <persianpros@yahoo.com>
2020-01-18 14:59:24 +0330

    Remove what relies on our kernel module when Open Vision is in multiboot otherwise image crashes into endless loops.

--- mytest.py
+++ mytest.py
@@ -21,6 +21,14 @@ enigma.eTimer = eBaseImpl.eTimer
 enigma.eSocketNotifier = eBaseImpl.eSocketNotifier
 enigma.eConsoleAppContainer = eConsoleImpl.eConsoleAppContainer
 
+from Components.SystemInfo import SystemInfo
+if not SystemInfo["OpenVisionModule"]:
+	print "[mytest] Open Vision in multiboot! Now we have to remove what relies on our kernel module!"
+	from Components.Console import Console
+	Console = Console()
+	Console.ePopen('opkg remove enigma2-plugin-extensions-e2iplayer')
+	print "[mytest] Removed, this is on you not us!"
+
 from traceback import print_exc
 
 if getHaveMultiLib() == "True":
@@ -35,8 +43,6 @@ Components.ClientMode.InitClientMode()
 profile("SimpleSummary")
 from Screens import InfoBar
 from Screens.SimpleSummary import SimpleSummary
-
-from sys import stdout
 
 profile("Bouquets")
 from Components.config import config, configfile, ConfigText, ConfigYesNo, ConfigInteger, ConfigSelection, NoSave
@@ -417,7 +423,6 @@ class PowerKey:
 				except:
 					print "[mytest] error during executing module %s, screen %s" % (selected[1], selected[2])
 			elif selected[0] == "Menu":
-				from Screens.Menu import MainMenu, mdom
 				root = mdom.getroot()
 				for x in root.findall("menu"):
 					y = x.find("id")
@@ -516,7 +521,6 @@ def runScreenTest():
 	profile("Init:PowerKey")
 	power = PowerKey(session)
 
-	from Components.SystemInfo import SystemInfo
 	from enigma import getBoxType
 	if SystemInfo in ["FirstCheckModel","SecondCheckModel","ThirdCheckModel","DifferentLCDSettings"] or getBoxType() in ("alphatriplehd","tmtwin4k","osminiplus","sf3038","spycat","et7x00","ebox5000","ebox7358","eboxlumi","maram9","sezam5000hd","mbtwin","sezam1000hd","mbmini","atemio5x00","beyonwizt3","dinoboth265","axashistwin"):
 		profile("VFDSYMBOLS")
@@ -535,7 +539,7 @@ def runScreenTest():
 	runReactor()
 
 	profile("wakeup")
-	from time import time, strftime, localtime
+	from time import strftime, localtime
 	from Tools.StbHardware import setFPWakeuptime, setRTCtime
 	from Screens.SleepTimerEdit import isNextWakeupTime
 	#get currentTime
