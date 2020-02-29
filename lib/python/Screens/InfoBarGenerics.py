ab1c72856afb29196576d36f47faea3aa3a7cbc4
RAED <rrrr53@hotmail.com>
2020-02-29 03:38:34 +0300

    [InfoBarGenerics] Fix crash (OverflowError: Python int too large to convert to C long)

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -604,7 +604,7 @@ class NumberZap(Screen):
 
 	def keyBlue(self):
 		if config.misc.zapkey_delay.value > 0:
-			self.Timer.start(int(1000*config.misc.zapkey_delay.value), True)
+			self.Timer.start(int(1000*int(config.misc.zapkey_delay.value)), True)
 		if self.searchNumber:
 			if self.startBouquet == self.bouquet:
 				self.service, self.bouquet = self.searchNumber(int(self["number"].getText()), firstBouquetOnly = True)
@@ -615,7 +615,7 @@ class NumberZap(Screen):
 
 	def keyNumberGlobal(self, number):
 		if config.misc.zapkey_delay.value > 0:
-			self.Timer.start(int(1000*config.misc.zapkey_delay.value), True)
+			self.Timer.start(int(1000*int(config.misc.zapkey_delay.value)), True)
 		self.numberString += str(number)
 		self["number"].text = self["number_summary"].text = self.numberString
 
@@ -662,7 +662,7 @@ class NumberZap(Screen):
 		self.Timer = eTimer()
 		self.Timer.callback.append(self.keyOK)
 		if config.misc.zapkey_delay.value > 0:
-			self.Timer.start(int(1000*config.misc.zapkey_delay.value), True)
+			self.Timer.start(int(1000*int(config.misc.zapkey_delay.value)), True)
 
 class InfoBarNumberZap:
 	""" Handles an initial number for NumberZapping """
