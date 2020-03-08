f5e65ccc50ab319e57343e7923087dc63029a59e
RAED <rrrr53@hotmail.com>
2020-03-08 20:39:39 +0300

    [InfoBarGenerics] Fix crass (AttributeError: 'InfoBar' object has no attribute 'event')

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -1266,11 +1266,7 @@ class InfoBarEPG:
 			answer[1]()
 
 	def openSimilarList(self, eventid, refstr):
-		id = self.event
-		refstr = str(self.currentService)
-		if id is not None:
-			self.hide()
-			self.session.open(EPGSelection, refstr, None, id)
+		self.session.open(EPGSelection, refstr, None, eventid)
 
 	def getNowNext(self):
 		epglist = [ ]
