dd8692a47312c5f59f757853aef9af450caf0ee4
norhap <alvaro@elfogondesegovia.com>
2020-03-13 21:38:56 +0100

    Fix BSOD for undefined "channel" terrestrial

--- lib/python/Tools/Transponder.py
+++ lib/python/Tools/Transponder.py
@@ -242,7 +242,7 @@ def ConvertToHumanReadable(tp, tunertype = None):
 			eDVBFrontendParametersTerrestrial.System_DVB_T_T2 : "DVB-T/T2",
 			eDVBFrontendParametersTerrestrial.System_DVB_T : "DVB-T",
 			eDVBFrontendParametersTerrestrial.System_DVB_T2 : "DVB-T2"}.get(tp.get("system"))
-		ret["channel"] = _("CH%s") % channel if channel else ""
+		ret["channel"] = _("CH%s") % getChannelNumber(tp.get("frequency"), "DVB-T")
 	elif tunertype == "ATSC":
 		ret["tuner_type"] = "ATSC"
 		ret["modulation"] = {
