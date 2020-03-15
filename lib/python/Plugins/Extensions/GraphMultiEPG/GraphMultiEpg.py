9d8e358566571878f9d01fced4806c9bc30f81d4
norhap <alvaro@elfogondesegovia.com>
2020-03-15 22:41:41 +0100

    fix Various BSOD in Menu GraphMultiEpg
    *if you add it for runPlugin. __call__ in InforBarGenerics in the plugin sessions you have to add them too.
    
    https://github.com/OpenVisionE2/enigma2-openvision/commit/cc223aaedcb98487ba43a0ea56e6b85b634f5524#diff-438a591a356903a228b7209093a2d35e

--- lib/python/Plugins/Extensions/GraphMultiEPG/GraphMultiEpg.py
+++ lib/python/Plugins/Extensions/GraphMultiEPG/GraphMultiEpg.py
@@ -1145,7 +1145,7 @@ class GraphMultiEPG(Screen, HelpableScreen):
 
 	def runPlugin(self, plugin):
 		event = self["list"].getCurrent()
-		plugin(session=self.session, selectedevent=event)
+		plugin.__call__(session=self.session, selectedevent=event)
 
 	def openTimerOverview(self):
 		self.session.open(TimerEditList)
