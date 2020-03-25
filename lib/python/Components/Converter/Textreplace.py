6967bb7090797ed860fac298344643ecbb3fe9cf
RAED <rrrr53@hotmail.com>
2020-03-25 21:51:53 +0300

    [Components] Add some Converter and Renderer
    [Renderer]
    -AudioIcon
    [Converter]
    -VAudioInfo
    -TextAddAfter
    -TextAddBefore
    -Textreplace
    -TextToUpper

---
+++ lib/python/Components/Converter/Textreplace.py
@@ -0,0 +1,22 @@
+from Components.Converter.Converter import Converter
+from Components.Element import cached
+class Textreplace(Converter, object):
+	def __init__(self, type):
+		Converter.__init__(self, type)
+		self.type = type
+	
+	@cached
+	def getText(self):
+		if self.type is not None:
+			if self.source is not None:
+				if self.source.text is not None:
+					return self.source.text.replace(self.type, "")
+				else:
+					return ""
+			else:
+				return ""
+		else:
+			return ""
+		
+	text = property(getText)
+		
