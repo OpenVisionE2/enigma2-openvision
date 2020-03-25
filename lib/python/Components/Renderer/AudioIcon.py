43c16c1c9f4f4642d81c213bf447902db747b5c0
Hains <hainsvdbosch@ziggo.nl>
2020-03-25 21:14:09 +0100

    AudioIcon.py: Fix Renderer import

--- lib/python/Components/Renderer/AudioIcon.py
+++ lib/python/Components/Renderer/AudioIcon.py
@@ -1,4 +1,4 @@
-from Components.Renderer import Renderer
+from Components.Renderer.Renderer import Renderer
 from enigma import ePixmap
 from Tools.Directories import fileExists, SCOPE_CURRENT_SKIN, resolveFilename
 
