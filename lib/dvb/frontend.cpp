5098a16758fb29c28d9805124efe7019f8440147
persianpros <persianpros@yahoo.com>
2020-04-01 03:14:17 +0430

    frontend.cpp: Improve System_DVB_T_T2 handling

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -3121,6 +3121,11 @@ int eDVBFrontend::isCompatibleWith(ePtr<iDVBFrontendParameters> &feparm)
 			/* prefer to use a T tuner, try to keep T2 free for T2 transponders */
 			score--;
 		}
+		if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T_T2 && can_handle_dvbt2)
+		{
+			// System_DVB_T_T2 is a generic T/T2 type, so we prefer a dvb-t2 tuner
+			score++;
+		}
 	}
 	else if (type == eDVBFrontend::feATSC)
 	{
