c78a4d6a8e98e9a94b37deaa3dd5de46e66bb1a7
RAED <rrrr53@hotmail.com>
2020-04-18 23:26:57 +0300

    Fix crash ....
    File "/usr/lib/enigma2/python/Screens/ChannelSelection.py", line 1058, in addBouquet
    new_bouquet_ref.setPath('FROM BOUQUET "userbouquet.%s.tv" ORDER BY bouquet' % self.buildBouquetID(bName))
    [-] AttributeError: 'ChannelSelection' object has no attribute 'buildBouquetID'

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -1003,6 +1003,16 @@ class ChannelSelectionEdit:
 				break
 			cnt+=1
 
+	def buildBouquetID(self, str):
+		tmp = str.lower()
+		name = ''
+		for c in tmp:
+			if ('a' <= c <= 'z') or ('0' <= c <= '9'):
+				name += c
+			else:
+				name += '_'
+		return name
+
 	def addAlternativeServices(self):
 		cur_service = ServiceReference(self.getCurrentSelection())
 		end = self.atEnd()
