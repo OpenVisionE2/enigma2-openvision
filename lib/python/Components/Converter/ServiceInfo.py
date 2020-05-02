452375bb379347e2b8a9ce67dffadd192cfd0af0
Hains <hainsvdbosch@ziggo.nl>
2020-05-02 09:10:39 +0200

    ServiceInfo.py: Check videoHeight
    File "/usr/lib/enigma2/python/Components/Element.py", line 18, in wrapper
        cache[name] = (True, f(self))
      File "/usr/lib/enigma2/python/Components/Converter/ServiceInfo.py", line 180, in getBoolean
        return self.videoHeight >= 720 and self.videoHeight < 2100
    TypeError: '>=' not supported between instances of 'NoneType' and 'int'

--- lib/python/Components/Converter/ServiceInfo.py
+++ lib/python/Components/Converter/ServiceInfo.py
@@ -184,29 +184,30 @@ class ServiceInfo(Converter):
 				else:
 					videoHeight = info.getInfo(iServiceInformation.sVideoHeight)
 					self.videoHeight = videoHeight if videoHeight > 0 else self.videoHeight
-					if self.type == self.IS_SD:
-						return self.videoHeight and self.videoHeight < 720
-					elif self.type == self.IS_HD:
-						return self.videoHeight >= 720 and self.videoHeight <= 1080
-					elif self.type == self.IS_4K:
-						return self.videoHeight > 2100 and self.videoHeight <= 2160
-					elif self.type == self.IS_1080:
-						return self.videoHeight > 1000 and self.videoHeight <= 1080
-					elif self.type == self.IS_720:
-						return video_height > 700 and video_height <= 720
-					elif self.type == self.IS_576:
-						return video_height > 500 and video_height <= 576
-					elif self.type == self.IS_480:
-						return video_height > 0 and video_height <= 480
-					elif self.type == self.IS_HDHDR:
-						return self.videoHeight > 721 and self.videoHeight <= 1980
-					else:
-						aspect = info.getInfo(iServiceInformation.sAspect)
-						self.aspect = aspect if aspect > -1 else self.aspect
-						if self.type == self.IS_SD_AND_WIDESCREEN:
-							return self.videoHeight and self.aspect and self.videoHeight < 720 and self.aspect in WIDESCREEN
-						if self.type == self.IS_SD_AND_NOT_WIDESCREEN:
-							return self.videoHeight and self.aspect and self.videoHeight < 720 and self.aspect not in WIDESCREEN
+					if self.videoHeight is not None:
+						if self.type == self.IS_SD:
+							return self.videoHeight and self.videoHeight < 720
+						elif self.type == self.IS_HD:
+							return self.videoHeight >= 720 and self.videoHeight <= 1080
+						elif self.type == self.IS_4K:
+							return self.videoHeight > 2100 and self.videoHeight <= 2160
+						elif self.type == self.IS_1080:
+							return self.videoHeight > 1000 and self.videoHeight <= 1080
+						elif self.type == self.IS_720:
+							return video_height > 700 and video_height <= 720
+						elif self.type == self.IS_576:
+							return video_height > 500 and video_height <= 576
+						elif self.type == self.IS_480:
+							return video_height > 0 and video_height <= 480
+						elif self.type == self.IS_HDHDR:
+							return self.videoHeight > 721 and self.videoHeight <= 1980
+						else:
+							aspect = info.getInfo(iServiceInformation.sAspect)
+							self.aspect = aspect if aspect > -1 else self.aspect
+							if self.type == self.IS_SD_AND_WIDESCREEN:
+								return self.videoHeight and self.aspect and self.videoHeight < 720 and self.aspect in WIDESCREEN
+							if self.type == self.IS_SD_AND_NOT_WIDESCREEN:
+								return self.videoHeight and self.aspect and self.videoHeight < 720 and self.aspect not in WIDESCREEN
 		return False
 
 	boolean = property(getBoolean)
