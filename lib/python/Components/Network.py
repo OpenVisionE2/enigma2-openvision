ecdb4503708aa5ec24c7a5d1b7495593a5a45771
Hains <hainsvdbosch@ziggo.nl>
2020-05-02 09:12:56 +0200

    Network.py: Use str instead of integer
    cdir is '24tr', so str type.
    
    File "/usr/lib/enigma2/python/Components/Network.py", line 704, in calc_netmask
        print('[Network] cidr invalid: %d' % cidr)
    TypeError: %d format: a number is required, not str

--- lib/python/Components/Network.py
+++ lib/python/Components/Network.py
@@ -630,7 +630,7 @@ class Network:
 		cidr_range = range(0, 32)
 		cidr = long(nmask)
 		if cidr not in cidr_range:
-			print('[Network] cidr invalid: %d' % cidr)
+			print('[Network] cidr invalid: %str' % cidr)
 			return None
 		else:
 			nm = ((1L<<cidr)-1)<<(32-cidr)
