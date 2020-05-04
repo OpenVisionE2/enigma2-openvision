6c91450adf311665d1d0e03c120f1e1c21586fbb
Jörg Bleyel <web@bleyel.de>
2020-05-04 22:49:40 +0200

    Improve offline_decode_delay input

--- lib/python/Components/RecordingConfig.py
+++ lib/python/Components/RecordingConfig.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from Components.config import ConfigNumber, ConfigYesNo, ConfigSubsection, ConfigSelection, config
+from Components.config import ConfigInteger, ConfigNumber, ConfigYesNo, ConfigSubsection, ConfigSelection, config
 
 def InitRecordingConfig():
 	config.recording = ConfigSubsection()
@@ -18,6 +18,6 @@ def InitRecordingConfig():
 		("long", _("Long filenames")) ] )
 	config.recording.always_ecm = ConfigYesNo(default = False)
 	config.recording.never_decrypt = ConfigYesNo(default = False)
-	config.recording.offline_decode_delay = ConfigNumber(default = 1000)
+	config.recording.offline_decode_delay = ConfigInteger(default = 1000, limits=(1,10000))
 	config.recording.timer_default_type = ConfigSelection(choices = [("zap", _("zap")), ("record", _("record")), ("zap+record", _("zap and record"))], default = "record")
 	config.recording.show_rec_symbol_for_rec_types = ConfigSelection(choices = [("any", _("any recordings")), ("real", _("real recordings")), ("real_streaming", _("real recordings or streaming")), ("real_pseudo", _("real or pseudo recordings"))], default = "real_streaming")
