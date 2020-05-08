fb89d4e4b62c47795281c7cf06b51d26a1504d1f
persianpros <persianpros@yahoo.com>
2020-05-09 00:31:48 +0430

    pmt.cpp: Increase PAT chance for Amlogic

--- lib/dvb/pmt.cpp
+++ lib/dvb/pmt.cpp
@@ -245,7 +245,7 @@ void eDVBServicePMTHandler::PATready(int)
 		}
 		if (pmtpid == -1) {
 			eDebug("[eDVBServicePMTHandler] no PAT entry found.. start delay");
-#ifdef AZBOX
+#if defined(HAVE_AMLOGIC) || defined(AZBOX)
 			m_no_pat_entry_delay->start(10000, true);
 #else
 			m_no_pat_entry_delay->start(1000, true);
