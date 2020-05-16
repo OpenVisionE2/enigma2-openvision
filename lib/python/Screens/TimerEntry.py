e658e29f2e7e7c7238bdfdafc47872811dac67d4
RAED <rrrr53@hotmail.com>
2020-05-16 23:52:17 +0300

    [TimerEntry] Revert this commit https://github.com/OpenVisionE2/enigma2-openvision/commit/a1f2d17f0909c124ec67168acc4448cc1d30b21c#
    Fix crash ..
    
     File "/usr/lib/enigma2/python/Screens/TimerEntry.py", line 329, in keySelect
     NameError: global name 'ChannelSelection' is not defined
     [ePyObject] (CallObject(<bound method NumberActionMap.action of <Components.ActionMap.NumberActionMap instance at 0xaeda0210>>,('SetupActions', 'ok')) failed)

--- lib/python/Screens/TimerEntry.py
+++ lib/python/Screens/TimerEntry.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 from Screens.Screen import Screen
-import Screens.ChannelSelection
+import ChannelSelection
 from ServiceReference import ServiceReference
 from Components.config import config, ConfigSelection, ConfigText, ConfigSubList, ConfigDateTime, ConfigClock, ConfigYesNo, getConfigListEntry
 from Components.ActionMap import NumberActionMap
