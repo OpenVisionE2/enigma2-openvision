8e5f6e054f4e4432ac63284062cf746cecb3ed04
Persian Prince <persianpros@yahoo.com>
2020-08-14 00:14:49 +0430

    First use of getRCName and make things more simple.
    Also don't install extra remotes, we manage this with the new variable in OE.

--- lib/python/Components/RcModel.py
+++ lib/python/Components/RcModel.py
@@ -4,34 +4,22 @@ import os
 from enigma import getBoxType
 from Tools.Directories import SCOPE_SKIN, resolveFilename
 from Tools.StbHardware import getFPVersion, getBoxProc
+from boxbranding import getRCName
 
 fp_version = str(getFPVersion())
 procmodel = getBoxProc()
 
 class RcModel:
-	RcModels = {}
-
 	def __init__(self):
 		self.model = getBoxType()
-		# cfg files has modelname rcname entries.
-		# modelname is boxname optionally followed by .rctype
-		for line in open((resolveFilename(SCOPE_SKIN, 'rc_models/rc_models.cfg')), 'r'):
-			if line.startswith(self.model):
-				m, r = line.strip().split()
-				self.RcModels[m] = r
 
 	def rcIsDefault(self):
 		# Default RC can only happen with DMM type remote controls...
 		return self.model.startswith('dm')
 
 	def getRcFile(self, ext):
-		if self.model in self.RcModels.keys():
-			remote = self.RcModels[self.model]
-		elif self.model == "azboxhd" and not procmodel in ("elite","ultra"):
-			remote = "azboxhd"
-		elif procmodel in ("elite","ultra"):
-			remote = "azboxelite"
-		elif self.model == "et9x00" and not procmodel == "et9500":
+		remote = getRCName()
+		if self.model == "et9x00" and not procmodel == "et9500":
 			remote = "et9x00"
 		elif procmodel == "et9500":
 			remote = "et9500"
@@ -39,14 +27,16 @@ class RcModel:
 			remote = "et6x00"
 		elif procmodel == "et6500":
 			remote = "et6500"
+		elif self.model == "azboxhd" and not procmodel in ("elite","ultra"):
+			remote = "azboxhd"
+		elif procmodel in ("elite","ultra"):
+			remote = "azboxelite"
 		elif self.model == "ventonhdx" or procmodel == "ini-3000" and fp_version.startswith('1'):
 			remote = "ini0"
 		elif procmodel in ("ini-5000","ini-7000","ini-7012"):
 			remote = "ini1"
 		elif self.model == "ventonhdx" or procmodel == "ini-3000" and not fp_version.startswith('1'):
 			remote = "ini2"
-		else:
-			remote = "dmm1"	# default. Assume files for dmm1 exists
 		f = resolveFilename(SCOPE_SKIN, 'rc_models/' + remote + '.' + ext)
 		if not os.path.exists(f):
 			f = resolveFilename(SCOPE_SKIN, 'rc_models/dmm1.' + ext)
