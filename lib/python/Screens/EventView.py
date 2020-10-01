d3cde2c1421b8068001e136604ff59a1c9911f0e
norhap <elfogondesegovia@gmail.com>
2020-10-02 01:16:15 +0200

    EventView.py: Solved plugin call issue for eventname YT-trailer TMBD

--- lib/python/Screens/EventView.py
+++ lib/python/Screens/EventView.py
@@ -354,7 +354,7 @@ class EventViewBase:
 				self.session.openWithCallback(boxAction, ChoiceBox, title=text, list=menu, windowTitle=_("Event view context menu"))
 
 	def runPlugin(self, plugin):
-		plugin(session=self.session, service=self.currentService, event=self.event, eventName=self.event.getEventName())
+		plugin.__call__(session=self.session, service=self.currentService, event=self.event, eventName=self.event.getEventName())
 
 class EventViewSimple(Screen, EventViewBase):
 	def __init__(self, session, Event, Ref, callback=None, similarEPGCB=None, parent=None):
