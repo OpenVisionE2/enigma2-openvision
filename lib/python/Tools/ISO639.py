8e9f2093240e0960c6f379d712646b7c8b6d57d4
Persian Prince <persianpros@yahoo.com>
2020-10-18 13:33:10 +0330

    ISO639.py: Support loading python 2 pickle files

--- lib/python/Tools/ISO639.py
+++ lib/python/Tools/ISO639.py
@@ -5,5 +5,10 @@ try:
 except:
 	import pickle
 import enigma
+import six
+
 with open(enigma.eEnv.resolve("${datadir}/enigma2/iso-639-3.pck"), 'rb') as f:
-	LanguageCodes = pickle.load(f)
+	if six.PY2:
+		LanguageCodes = pickle.load(f)
+	else:
+		LanguageCodes = pickle.load(f, encoding="bytes")
