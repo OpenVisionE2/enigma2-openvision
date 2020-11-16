79f7925a8c97ea03d710bd35e89aceeff9279180
norhap <elfogondesegovia@gmail.com>
2020-11-16 14:16:27 +0100

    PowerTimerList.py: we adjust icon position and state
    with this we do not cut the state text in long translations, we position the icon so that it looks complete

--- lib/python/Components/PowerTimerList.py
+++ lib/python/Components/PowerTimerList.py
@@ -114,9 +114,9 @@ class PowerTimerList(GUIComponent, object):
 		if timer.failed:
 			state = _("failed")
 			icon = self.iconFailed
-		icon and res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHABLEND, self.iconMargin / 2, (self.rowSplit - self.iconHeight) / 2, self.iconWidth, self.iconHeight, icon))
+		icon and res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHABLEND, self.iconMargin / 6, (self.rowSplit - self.iconHeight) / 6, self.iconWidth, self.iconHeight, icon))
 
-		res.append((eListboxPythonMultiContent.TYPE_TEXT, self.iconMargin + self.iconWidth, self.rowSplit, 150, height - self.rowSplit, 2, RT_HALIGN_LEFT|RT_VALIGN_BOTTOM, state))
+		res.append((eListboxPythonMultiContent.TYPE_TEXT, self.iconMargin + self.iconWidth, self.rowSplit, 400, height - self.rowSplit, 2, RT_HALIGN_LEFT|RT_VALIGN_BOTTOM, state))
 
 
 		line = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "div-h.png"))
