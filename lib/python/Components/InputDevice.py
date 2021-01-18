58a8ae4fb87f6bea20eab32943e98025eae9ecf9
persianpros <persianpros@yahoo.com>
2021-01-18 15:43:00 +0330

    InputDevice.py: Don't accept front panel and cec_input

--- lib/python/Components/InputDevice.py
+++ lib/python/Components/InputDevice.py
@@ -63,6 +63,8 @@ class inputDevices:
 				self.name = None
 
 			if self.name:
+				if "front panel" or "cec_input" in self.name:
+					continue
 				self.Devices[evdev] = {'name': self.name, 'type': self.getInputDeviceType(self.name),'enabled': False, 'configuredName': None }
 				if model.startswith("et"):
 					self.setDefaults(evdev)
