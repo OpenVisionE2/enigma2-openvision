006f145e36e5e3e5e27646c3c8a1efc03df8f4b2
persianpros <persianpros@yahoo.com>
2021-03-04 19:54:53 +0330

    Revert "Add a global scrollbar style (C++ part only)"
    This reverts commit a1a2b4430df3d93d0cf481bf90c69c8451cf7791.

--- lib/gui/elistbox.h
+++ lib/gui/elistbox.h
@@ -61,10 +61,9 @@ struct eListboxStyle
 {
 	ePtr<gPixmap> m_background, m_selection;
 	int m_transparent_background;
-	gRGB m_background_color, m_background_color_selected, m_foreground_color, m_foreground_color_selected, m_border_color;
-	int m_background_color_set, m_foreground_color_set, m_background_color_selected_set, m_foreground_color_selected_set;
-	gRGB m_scrollbarborder_color, m_scrollbarforeground_color, m_scrollbarbackground_color;
-	int m_scrollbarforeground_color_set, m_scrollbarbackground_color_set, m_scrollbarborder_color_set, m_scrollbarborder_width_set;
+	gRGB m_background_color, m_background_color_selected,
+	m_foreground_color, m_foreground_color_selected, m_border_color, m_sliderborder_color, m_sliderforeground_color;
+	int m_background_color_set, m_foreground_color_set, m_background_color_selected_set, m_foreground_color_selected_set, m_sliderforeground_color_set, m_sliderborder_color_set, m_scrollbarsliderborder_size_set;
 		/*
 			{m_transparent_background m_background_color_set m_background}
 			{0 0 0} use global background color
@@ -83,7 +82,7 @@ struct eListboxStyle
 		alignBottom=alignRight,
 		alignBlock
 	};
-	int m_valign, m_halign, m_border_size, m_scrollbarborder_width;
+	int m_valign, m_halign, m_border_size, m_sliderborder_size, m_scrollbarsliderborder_size;
 	ePtr<gFont> m_font, m_secondfont;
 	ePoint m_text_offset;
 };
@@ -149,19 +148,20 @@ public:
 	void setBackgroundPicture(ePtr<gPixmap> &pixmap);
 	void setSelectionPicture(ePtr<gPixmap> &pixmap);
 
+	void setSliderPicture(ePtr<gPixmap> &pm);
+	void setScrollbarBackgroundPicture(ePtr<gPixmap> &pm);
+	void setScrollbarSliderBorderWidth(int size);
+	void setScrollbarWidth(int size);
+
 	void setFont(gFont *font);
 	void setSecondFont(gFont *font);
 	void setVAlign(int align);
 	void setHAlign(int align);
 	void setTextOffset(const ePoint &textoffset);
 
-	void setScrollbarWidth(int width);
-	void setScrollbarBorderWidth(int width);
-	void setScrollbarBorderColor(const gRGB &col);
-	void setScrollbarForegroundColor(const gRGB &col);
-	void setScrollbarBackgroundColor(const gRGB &col);
-	void setScrollbarPixmap(ePtr<gPixmap> &pm);
-	void setScrollbarBackgroundPixmap(ePtr<gPixmap> &pm);
+	void setSliderBorderColor(const gRGB &col);
+	void setSliderBorderWidth(int size);
+	void setSliderForegroundColor(gRGB &col);
 
 	int getScrollbarWidth() { return m_scrollbar_width; }
 
