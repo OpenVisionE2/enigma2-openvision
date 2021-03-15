1eb718e1565d0b7150bea95e44c0e997a71ed0c4
persianpros <persianpros@yahoo.com>
2021-03-15 12:49:08 +0330

    PEP8 double aggressive E301 ~ E306

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -6,7 +6,6 @@ class CommitInfo(CommitLogInformation):  # Entry point for legacy code that is y
 		CommitLogInformation.__init__(self, session)
 
 
-
 class MemoryInfo(MemoryInformation):  # Entry point for legacy code that is yet to be updated.
 	def __init__(self, session):
 		CommitLogInformation.__init__(self, session)
