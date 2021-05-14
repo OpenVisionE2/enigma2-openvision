75e7199efef584663d2c64ca4bf2de6841944461
IanSav <IanSav@users.noreply.github.com>
2021-05-15 04:06:41 +1000

    [keymapparser.py] Restore a redirector version
    * [keymapparser.py] Restore a redirector version
    
    This module has been restored to allow old code and plugins to continue to function until such time as they are updated to use the newer modules and methods directly.
    
    * [Makefile.am] Add keymapparser.py redirector

---
+++ lib/python/keymapparser.py
@@ -0,0 +1,24 @@
+# This module is a placeholder / redirector to allow old code and plugins
+# to continue to function until such time that they are updated to use the
+# newer modules and methods.
+
+from ActionMap import loadKeymap, parseKeymap, removeKeymap as removeKeymapNew
+
+class KeymapError(Exception):
+	def __init__(self, message):
+		self.msg = message
+
+	def __str__(self):
+		return self.msg
+
+
+def parseKeys(context, filename, actionmap, device, keys):
+	return parseKeymap(filename, context, actionmap, device, keys)
+
+
+def readKeymap(filename):
+	return loadKeymap(filename)
+
+
+def removeKeymap(filename):
+	return removeKeymapNew(filename)
