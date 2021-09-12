3deee72471c6abb99bfd3baa927b91bc0a297229
IanSav <IanSav@users.noreply.github.com>
2021-09-12 18:08:30 +1000

    [WizardLanguage.py] Correct "key_red" to StaticText

--- lib/python/Screens/WizardLanguage.py
+++ lib/python/Screens/WizardLanguage.py
@@ -1,4 +1,5 @@
 from Components.Label import Label
+from Components.Sources.StaticText import StaticText
 from Screens.LocaleSelection import LocaleWizard
 from Screens.Wizard import Wizard
 
@@ -6,7 +7,7 @@ from Screens.Wizard import Wizard
 class WizardLanguage(Wizard):
 	def __init__(self, session, showSteps=True, showStepSlider=True, showList=True, showConfig=True):
 		Wizard.__init__(self, session, showSteps, showStepSlider, showList, showConfig)
-		self["key_red"] = Label(_("Locale/Language"))
+		self["key_red"] = StaticText(_("Locale/Language"))
 		self["languagetext"] = Label(_("Change Language"))
 
 	def red(self):
