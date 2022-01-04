8f65967620c6dba866778e8e10ce17515b36ca9d
persianpros <persianpros@gmail.com>
2022-01-04 18:55:52 +0330

    [ChoiceList.py] Optimise and improve code
    - Optimise and sort the imports.
    - Vertically center the text on the line.
    
    [ChoiceList.py] Improve argument processing
    
    Co-authored-by: IanSav <is.ozpvr@gmail.com>
    Co-authored-by: jbleyel <jbleyel@gmx.net>

--- lib/python/Components/ChoiceList.py
+++ lib/python/Components/ChoiceList.py
@@ -1,21 +1,22 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+from enigma import RT_HALIGN_LEFT, RT_VALIGN_CENTER, eListboxPythonMultiContent, gFont
+
+from skin import fonts, parameters
 from Components.MenuList import MenuList
 from Tools.Directories import SCOPE_GUISKIN, resolveFilename
-from enigma import RT_HALIGN_LEFT, eListboxPythonMultiContent, gFont
 from Tools.LoadPixmap import LoadPixmap
-import skin
 
 
-def ChoiceEntryComponent(key=None, text=["--"]):
+def ChoiceEntryComponent(key=None, text=None):
+	if text is None:
+		text = ["--"]
 	res = [text]
 	if text[0] == "--":
-		x, y, w, h = skin.parameters.get("ChoicelistDash", (0, 0, 800, 25))
-		res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT, "-" * 200))
+		x, y, w, h = parameters.get("ChoicelistDash", (0, 0, 800, 25))
+		res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, "-" * 200))
 	else:
 		if key:
-			x, y, w, h = skin.parameters.get("ChoicelistName", (45, 0, 800, 25))
-			res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT, text[0]))
+			x, y, w, h = parameters.get("ChoicelistName", (45, 0, 800, 25))
+			res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, text[0]))
 			if key == "dummy":
 				png = None
 			elif key == "expandable":
@@ -29,18 +30,18 @@ def ChoiceEntryComponent(key=None, text=["--"]):
 			else:
 				png = LoadPixmap(resolveFilename(SCOPE_GUISKIN, "buttons/key_%s.png" % key))
 			if png:
-				x, y, w, h = skin.parameters.get("ChoicelistIcon", (5, 0, 35, 25))
+				x, y, w, h = parameters.get("ChoicelistIcon", (5, 0, 35, 25))
 				res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHABLEND, x, y, w, h, png))
 		else:
-			x, y, w, h = skin.parameters.get("ChoicelistNameSingle", (5, 0, 800, 25))
-			res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT, text[0]))
+			x, y, w, h = parameters.get("ChoicelistNameSingle", (5, 0, 800, 25))
+			res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, text[0]))
 	return res
 
 
 class ChoiceList(MenuList):
 	def __init__(self, list, selection=0, enableWrapAround=False):
 		MenuList.__init__(self, list, enableWrapAround, eListboxPythonMultiContent)
-		font = skin.fonts.get("ChoiceList", ("Regular", 20, 30))
+		font = fonts.get("ChoiceList", ("Regular", 20, 30))
 		self.l.setFont(0, gFont(font[0], font[1]))
 		self.l.setItemHeight(font[2])
 		self.selection = selection
