ad358d94beec18098cabe46490a99d247ad53b2c
persianpros <persianpros@yahoo.com>
2022-02-09 14:50:43 +0330

    Network.py: Improvements
    Co-authored-by: WanWizard <wanwizard@wanwizard.eu>

--- lib/python/Components/Network.py
+++ lib/python/Components/Network.py
@@ -77,9 +77,11 @@ class Network:
 	def getAddrInet(self, iface, callback):
 		data = {'up': False, 'dhcp': False, 'preup': False, 'predown': False}
 		try:
-			print("[Network] Read /sys/class/net/%s/flags" % iface)
-			data['up'] = int(open('/sys/class/net/%s/flags' % iface).read().strip(), 16) & 1 == 1
-			self.configuredInterfaces.append(iface)
+			print("[Network] Read /sys/class/net/%s/carrier" % iface)
+			if os.path.exists('/sys/class/net/%s/carrier' % iface):
+				data['up'] = open('/sys/class/net/%s/carrier' % iface).read().strip() == '1'
+			if data['up']:
+				self.configuredInterfaces.append(iface)
 			nit = ni.ifaddresses(iface)
 			data['ip'] = self.convertIP(nit[ni.AF_INET][0]['addr']) # ipv4
 			data['netmask'] = self.convertIP(nit[ni.AF_INET][0]['netmask'])
@@ -94,7 +96,10 @@ class Network:
 			data['ip'] = [0, 0, 0, 0]
 			data['netmask'] = [0, 0, 0, 0]
 			data['gateway'] = [0, 0, 0, 0]
-		self.ifaces[iface] = data
+		if iface in self.ifaces:
+			self.ifaces[iface].update(data)
+		else:
+			self.ifaces[iface] = data
 		self.loadNetworkConfig(iface, callback)
 
 	def writeNetworkConfig(self):
@@ -168,12 +173,13 @@ class Network:
 			split = i.strip().split(' ')
 			if split[0] == "iface" and split[2] != "inet6":
 				currif = split[1]
-				ifaces[currif] = {}
-				if len(split) == 4 and split[3] == "dhcp":
-					ifaces[currif]["dhcp"] = True
-				else:
-					ifaces[currif]["dhcp"] = False
 			if currif == iface: #read information only for available interfaces
+				if currif not in ifaces:
+					ifaces[currif] = {}
+					if len(split) == 4 and split[3] == "dhcp":
+						ifaces[currif]["dhcp"] = True
+					else:
+						ifaces[currif]["dhcp"] = False
 				if split[0] == "address":
 					ifaces[currif]["address"] = map(int, split[1].split('.'))
 					if "ip" in self.ifaces[currif]:
@@ -328,6 +334,9 @@ class Network:
 		return self.ifaces.keys()
 
 	def getAdapterAttribute(self, iface, attribute):
+		print("Getting attribute ", attribute, " for adapter", iface)
+		if self.ifaces.get(iface, {}).get('up', False) and self.ifaces.get(iface, {}).get('ip', [0, 0, 0, 0]) == [0, 0, 0, 0]:
+			self.getAddrInet(iface, None)
 		return self.ifaces.get(iface, {}).get(attribute)
 
 	def setAdapterAttribute(self, iface, attribute, value):
