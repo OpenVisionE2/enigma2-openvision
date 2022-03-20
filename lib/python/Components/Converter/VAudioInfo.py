2910362e3301d7671d445c0855e458b88c5e755a
RAED <rrrr53@hotmail.com>
2022-03-20 05:38:13 +0300

    VAudioInfo.py: Py3 fix TypeError
    str.translate() takes exactly one argument (2 given)

--- lib/python/Components/Converter/VAudioInfo.py
+++ lib/python/Components/Converter/VAudioInfo.py
@@ -66,7 +66,7 @@ class VAudioInfo(Poll, Converter, object):
 		return description_str
 
 	def getAudioIcon(self, info):
-		description_str = self.get_short(self.getAudioCodec(info).translate(None, ' .').lower())
+		description_str = self.get_short(self.getAudioCodec(info).translate(str.maketrans("", "", ' .')).lower())
 		return description_str
 
 	def get_short(self, audioName):
