67de8ff71526a20b073ce511cb71452620d21413
persianpros <persianpros@yahoo.com>
2022-05-21 19:24:46 +0430

    Add URL paramter to support alternative links
    Co-authored-by: jbleyel <jbleyel@gmx.net>

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -1862,7 +1862,6 @@ RESULT eDVBServicePlay::subServices(ePtr<iSubserviceList> &ptr)
 RESULT eDVBServicePlay::timeshift(ePtr<iTimeshiftService> &ptr)
 {
 	ptr = nullptr;
-	/* eDebug("[eDVBServicePlay] timeshift"); */
 	if (m_timeshift_enabled || !m_is_pvr)
 	{
 		if (!m_timeshift_enabled)
@@ -1890,8 +1889,10 @@ RESULT eDVBServicePlay::timeshift(ePtr<iTimeshiftService> &ptr)
 			}
 		}
 		ptr = this;
+		eTrace("[eDVBServicePlay] timeshift return 0");
 		return 0;
 	}
+	eTrace("[eDVBServicePlay] timeshift return -1");
 	return -1;
 }
 
