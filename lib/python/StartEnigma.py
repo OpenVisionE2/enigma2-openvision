542b7667c3f42009b57b4bc2dd1c657a2673feaa
norhap <elfogondesegovia@gmail.com>
2022-06-02 22:34:31 +0200

    StartEnigma.py: add config.clientmode_import_restart.value
    -  When clientMode is enabled, we can configure whether to run ChannelsImporter on enigma2 reboot or not.
     .-Then this parameter must be part of the condition to load bouquets, if we have it disabled in setup clientMode we must load eDVBDB.

--- lib/python/StartEnigma.py
+++ lib/python/StartEnigma.py
@@ -567,7 +567,7 @@ from Screens import InfoBar
 from Screens.SimpleSummary import SimpleSummary
 
 profile("Bouquets")
-if config.clientmode.enabled.value == False:
+if not config.clientmode.enabled.value or not config.clientmode_import_restart.value:
 	enigma.eDVBDB.getInstance().reloadBouquets()
 
 profile("ParentalControl")
