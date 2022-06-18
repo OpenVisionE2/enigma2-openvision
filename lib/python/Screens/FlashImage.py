9e3330b24c863baf57a825251770d19220f432d9
norhap <elfogondesegovia@gmail.com>
2022-06-18 02:17:00 +0200

    FlashImage.py: inherit a common clause for both python
    FileNotFoundError = python 3
    is not defined for python 2, use code common inherit.

--- lib/python/Screens/FlashImage.py
+++ lib/python/Screens/FlashImage.py
@@ -456,10 +456,11 @@ class MultiBootSelection(SelectImage):
 				self.session.open(TryQuitMainloop, 2)
 			else:
 				self.close(value)
-		except FileNotFoundError as err:
-			print("[FlashImage]: %s" % err)
-			from Screens.Standby import TryQuitMainloop
-			self.session.open(TryQuitMainloop, 3)
+		except Exception as err:
+			import errno
+			if err.errno == errno.ENOENT:
+				from Screens.Standby import TryQuitMainloop
+				self.session.open(TryQuitMainloop, 3)
 
 	def getImagesList(self):
 		list = []
