2e131db22190fff649b2a20deb772703daebe454
persianpros <persianpros@yahoo.com>
2022-07-05 18:56:14 +0430

    PowerTimerEdit.py: Update list compare method for any py version
    Co-authored-by: norhap <elfogondesegovia@gmail.com>

--- lib/python/Screens/PowerTimerEdit.py
+++ lib/python/Screens/PowerTimerEdit.py
@@ -1,4 +1,3 @@
-from six import PY2
 from time import time
 from timer import TimerEntry as RealTimerEntry
 
@@ -191,22 +190,20 @@ class PowerTimerEditList(Screen):
 			cb(time, duration, state)
 
 	def fillTimerList(self):
+		from functools import cmp_to_key
 		#helper function to move finished timers to end of list
+
 		def eol_compare(x, y):
 			if x[0].state != y[0].state and x[0].state == RealTimerEntry.StateEnded or y[0].state == RealTimerEntry.StateEnded:
-				return cmp(x[0].state, y[0].state)
-			return cmp(x[0].begin, y[0].begin)
+				return (x[0].state > y[0].state) - (x[0].state < y[0].state)
+			return (x[0].begin > y[0].begin) - (x[0].begin < y[0].begin)
 
 		list = self.list
 		del list[:]
 		list.extend([(timer, False) for timer in self.session.nav.PowerTimer.timer_list])
 		list.extend([(timer, True) for timer in self.session.nav.PowerTimer.processed_timers])
 		if config.usage.timerlist_finished_timer_position.index: #end of list
-			if PY2:
-				list.sort(cmp=eol_compare)
-			else:
-				import functools
-				list.sort(key=functools.cmp_to_key(eol_compare))
+			list.sort(key=cmp_to_key(eol_compare))
 		else:
 			list.sort(key=lambda x: x[0].begin)
 
