c819c7dd6973e6d3bc4c663368a4e95d760b39c3
norhap <elfogondesegovia@gmail.com>
2022-07-08 21:33:21 +0200

    PictureInPicture - prevent crash(caught) due to delete of self.pipservice when None set
    Co-Authored-By: TwolDE2 <tonywhalley@t-online.de>

--- lib/python/Screens/PictureInPicture.py
+++ lib/python/Screens/PictureInPicture.py
@@ -85,7 +85,8 @@ class PictureInPicture(Screen):
 		self.onLayoutFinish.append(self.LayoutFinished)
 
 	def __del__(self):
-		del self.pipservice
+		if self.pipservice:
+			del self.pipservice
 		self.setExternalPiP(False)
 		self.setSizePosMainWindow()
 		if hasattr(self, "dishpipActive") and self.dishpipActive is not None:
@@ -198,6 +199,10 @@ class PictureInPicture(Screen):
 				self.currentServiceReference = None
 				if not config.usage.hide_zap_errors.value:
 					AddPopup(text="PiP...\n" + _("Connected transcoding, limit - no PiP!"), type=MessageBox.TYPE_ERROR, timeout=5, id="ZapPipError")
+				return False
+			if ref and "4097" in ref.toString():
+				self.pipservice = None
+				AddPopup(text=_("Service type 4097 incorrect for PiP!"), type=MessageBox.TYPE_ERROR, timeout=5, id="ZapPipError")
 				return False
 			if self.isPlayableForPipService(ref):
 				print("[PictureInPicture] playing pip service", ref and ref.toString())
