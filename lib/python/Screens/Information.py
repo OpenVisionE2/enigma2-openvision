1d4d189778290ed8dafe50afe15ecc0a23121735
AYP <persianpros@yahoo.com>
2022-07-29 13:00:59 +0430

    Update Information.py

--- lib/python/Screens/Information.py
+++ lib/python/Screens/Information.py
@@ -597,9 +597,7 @@ class ImageInformation(InformationBase):
 			ovUrl = "https://raw.githubusercontent.com/OpenVisionE2/revision/master/%s.conf" % ("new" if str(BoxInfo.getItem("imgversion")).startswith("10") else "old")
 			try:
 				ovResponse = urlopen(ovUrl)
-				from six import ensure_str
-				ovResponse = ensure_str(ovResponse)
-				ovRevision = ovResponse.read().strip()
+				ovRevision = str(ovResponse.read().decode()).strip()
 				ovRevisionUpdate = ovRevision[1:]
 			except Exception as err:
 				ovRevisionUpdate = _("Requires internet connection")
