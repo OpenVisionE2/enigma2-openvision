2fa8a9fd1f4323e4347e53b76da53cba1944ea63
norhap <elfogondesegovia@gmail.com>
2022-11-13 20:33:33 +0100

    Lcd.py: Use floor division for value

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -164,7 +164,7 @@ class LCD:
 
 	def setDimBright(self, value):
 		value *= 255
-		value /= 10
+		value //= 10
 		if value > 255:
 			value = 255
 		self.dimBrightness = value
@@ -174,7 +174,7 @@ class LCD:
 
 	def setContrast(self, value):
 		value *= 63
-		value /= 20
+		value //= 20
 		if value > 63:
 			value = 63
 		eDBoxLCD.getInstance().setLCDContrast(value)
