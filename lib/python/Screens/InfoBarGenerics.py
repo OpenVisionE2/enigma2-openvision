f04fa9940c0b402aed6c921ff5d780df160496e3
persianpros <persianpros@yahoo.com>
2022-12-24 20:00:28 +0330

    Subservices: fix Python 3.11.x problem
    - Also use /usr/share/enigma2/groupedservices as default instead of /etc/enigma2/groupedservices
    
    Co-authored-by: norhap <elfogondesegovia@gmail.com>

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -35,13 +35,11 @@ from Screens.TimeDateInput import TimeDateInput
 from Screens.UnhandledKey import UnhandledKey
 from ServiceReference import ServiceReference, isPlayableForCur, hdmiInServiceRef
 from Tools.ASCIItranslit import legacyEncode
-from Tools.Directories import fileExists, fileReadLine, fileWriteLine, getRecordingFilename, moveFiles, isPluginInstalled
+from Tools.Directories import fileExists, fileReadLine, fileWriteLine, fileReadLines, fileReadLinesISO, getRecordingFilename, moveFiles, isPluginInstalled
 from Tools.Notifications import AddNotificationWithCallback, AddPopup, current_notifications, lock, notificationAdded, notifications, RemovePopup, AddNotification
 from keyids import KEYFLAGS, KEYIDS, KEYIDNAMES
 from enigma import eTimer, eServiceCenter, eDVBServicePMTHandler, iServiceInformation, iPlayableService, eServiceReference, eEPGCache, eActionMap, getDesktop, eDVBDB
 from time import time, localtime, strftime
-import os
-from os import sys
 from os.path import isfile, splitext
 from bisect import insort
 import itertools
@@ -157,13 +155,14 @@ class subservice:
 
 def reload_subservice_groupslist(force=False):
 	if subservice.groupslist is None or force:
+		groupedservices = "/usr/share/enigma2/groupedservices" if isfile("/usr/share/enigma2/groupedservices") else "/etc/enigma2/groupedservices"
 		try:
-			groupedservices = "/etc/enigma2/groupedservices"
-			if not isfile(groupedservices):
-				groupedservices = "/usr/share/enigma2/groupedservices"
-			subservice.groupslist = [list(g) for k, g in itertools.groupby([line.split('#')[0].strip() for line in open(groupedservices).readlines()], lambda x:not x) if not k]
+			subservice.groupslist = [list(g) for k, g in itertools.groupby([line.split('#')[0].strip() for line in fileReadLines(groupedservices, source=MODULE_NAME)], lambda x:not x) if not k]
 		except:
-			subservice.groupslist = []
+			try:
+				subservice.groupslist = [list(g) for k, g in itertools.groupby([line.split('#')[0].strip() for line in fileReadLinesISO(groupedservices, source=MODULE_NAME)], lambda x:not x) if not k]
+			except:
+				subservice.groupslist = []
 
 
 reload_subservice_groupslist()
