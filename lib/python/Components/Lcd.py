6ca3a0179f101fc4e804634b1e7e72b718d16e7b
Captain <captain.onboard@web.de>
2023-07-22 21:43:49 +0200

    [Ci] Fix IndexError: list assignment index out of range

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -770,5 +770,7 @@ def InitLcd():
 		config.lcd.ledbrightnessdeepstandby = ConfigNothing()
 		config.lcd.ledbrightnessdeepstandby.apply = lambda: doNothing()
 		config.lcd.ledblinkingtime = ConfigNothing()
+		config.usage.lcd_standbypowerled = ConfigNothing()
+		config.usage.lcd_deepstandbypowerled = ConfigNothing()
 		config.lcd.picon_pack = ConfigNothing()
 	config.misc.standbyCounter.addNotifier(standbyCounterChanged, initial_call=False)
